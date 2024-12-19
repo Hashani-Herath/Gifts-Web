05cbe4fd72cdc8a6cc77fcb6c73dd7fad045b849
Hashani Herath <140096325+Hashani-Herath@users.noreply.github.com>
2024-12-19 12:28:18 +0530

    Create Product Server Response Function

--- backend/product-service/src/main/java/com/programmingtechie/product_service/controller/ProductController.java
+++ backend/product-service/src/main/java/com/programmingtechie/product_service/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.programmingtechie.product_service.controller;
 
 import com.programmingtechie.product_service.dto.ProductRequest;
+import com.programmingtechie.product_service.dto.ProductResponse;
 import com.programmingtechie.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -17,7 +20,12 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
         productService.createProduct(productRequest);
+    }//this is an endpoint to create the products
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 
 }
--- backend/product-service/src/main/java/com/programmingtechie/product_service/service/ProductService.java
+++ backend/product-service/src/main/java/com/programmingtechie/product_service/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.programmingtechie.product_service.service;
 
 import com.programmingtechie.product_service.dto.ProductRequest;
+import com.programmingtechie.product_service.dto.ProductResponse;
 import com.programmingtechie.product_service.model.Product;
 import com.programmingtechie.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,19 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved." ,product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
